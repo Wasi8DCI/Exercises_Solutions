f7fcfac919e1108fedb757b6926eb9b7e131447e
user-ThinkPad-L15-Gen-1 <zwgmxde@gmail.com>
2022-05-02 11:11:28 +0200

    some changes

--- Task 1.py
+++ Task 1.py
@@ -2,7 +2,7 @@ username = input("What is your username? ")
 password = input(f"Type the password for username {username}: ")
 valid = {"username": "admin", "password": "admin"}
 
-while password == "admin" and username == "admin":
+while password == valid["password"] and username == valid["username"]:
     print("Welcome, admin!")
     break
 else:
